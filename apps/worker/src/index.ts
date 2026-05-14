73cb514a5b04c8805a780886809589e2748065a1
TejasMG12 <tejasgampawar12@gmail.com>
2026-05-15 01:31:03 +0530

    deployment

--- apps/worker/src/index.ts
+++ apps/worker/src/index.ts
@@ -1,3 +1,4 @@
+import 'dotenv/config';
 import { createClient } from "redis";
 import { exec } from "child_process";
 import fs from "fs/promises";
@@ -24,6 +25,7 @@ if (cluster.isPrimary) {
   });
 } else {
   const redisUrl = process.env.REDIS_URL?.trim();
+  console.log('Current ENV:', process.env.REDIS_URL);
   const redisOpts = redisUrl ? { url: redisUrl } : undefined;
   const client = redisOpts ? createClient(redisOpts) : createClient();
   const pubClient = redisOpts ? createClient(redisOpts) : createClient();
